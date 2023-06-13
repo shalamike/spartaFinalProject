f4d3f726b88c5d404ab7ecbc52314cb7836b7f6b
shalamike <m.shalaby1994@gmail.com>
2023-06-13 09:57:48 +0100

    initial commit

---
+++ src/main/java/com/example/spartafinalproject/SpartaFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartafinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spartafinalproject/SpartaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartafinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
