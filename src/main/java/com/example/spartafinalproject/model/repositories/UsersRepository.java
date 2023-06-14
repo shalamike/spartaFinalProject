08a6484a0741e6d8d0dc16e62610b39f461c8dba
Eesa Mannan <emannan@spartaglobal.com>
2023-06-14 14:59:14 +0100

    CRUD methods added to User controller

--- src/main/java/com/example/spartafinalproject/model/repositories/UsersRepository.java
+++ src/main/java/com/example/spartafinalproject/model/repositories/UsersRepository.java
@@ -4,4 +4,12 @@ import com.example.spartafinalproject.model.dtos.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsersRepository extends MongoRepository<User, String> {
+
+    default User createUser(User user) {
+        return save(user);
+    }
+
+    default User updateUser(User user) {
+        return save(user);
+    }
 }
