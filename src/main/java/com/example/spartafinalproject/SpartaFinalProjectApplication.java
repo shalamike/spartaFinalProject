64a14bbfec9d8008fa3ef1db2a1af97fe0b28b50
Eesa Mannan <emannan@spartaglobal.com>
2023-06-14 11:40:58 +0100

    my files

--- src/main/java/com/example/spartafinalproject/SpartaFinalProjectApplication.java
+++ src/main/java/com/example/spartafinalproject/SpartaFinalProjectApplication.java
@@ -1,13 +1,24 @@
 package com.example.spartafinalproject;
 
+import com.example.spartafinalproject.model.respositories.UsersRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SpartaFinalProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpartaFinalProjectApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(UsersRepository repository){
+        return args -> {
+            System.out.println(repository.findById("59b99db4cfa9a34dcd7885b7").get());
+        };
+    }
 }
